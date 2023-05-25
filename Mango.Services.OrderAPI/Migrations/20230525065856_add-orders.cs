b70bd9001907d859bf54acb75e36b00f2f0f15b8
Ho-Zakizadeh <Ho-Zakizadeh@htsc.ir>
2023-05-25 12:00:04 +0330

    add Order and azure message bus
    add Order and azure message bus

---
+++ Mango.Services.OrderAPI/Migrations/20230525065856_add-orders.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mango.Services.OrderAPI.Migrations
+{
+    public partial class addorders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
